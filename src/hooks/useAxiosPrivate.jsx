257d3f46d977092cb31ac08aee455e48817d9fe9
Rifa9oct <kohinuraktherr@gmail.com>
2023-12-22 18:56:24 +0600

    deploy site

--- src/hooks/useAxiosPrivate.jsx
+++ src/hooks/useAxiosPrivate.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosPrivate = axios.create({
-    baseURL:"http://localhost:5000"
+    baseURL:"https://task-management-server-khaki.vercel.app"
 })
 
 const useAxiosPrivate = () => {
