f298d2157fe079f6cfb96af6d9f78c71daf90787
Rifa9oct <kohinuraktherr@gmail.com>
2023-12-23 23:39:07 +0600

    modify code

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -3,7 +3,7 @@ import img from "../../assets/banner.png"
 
 const Banner = () => {
     return (
-        <div className="flex top-0 flex-col-reverse lg:flex-row items-center justify-between pt-[150px] lg:pt-0 md:pt-[100px] pb-[80px] lg:pb-0 md:pb-[50px] lg:h-[700px] bg-blue-50 w-full px-[150px]">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-between pb-[80px] lg:pb-0 md:pb-[50px] lg:h-[600px] bg-blue-50 w-full px-[150px]">
             <div data-aos="fade-right">
                 <h1 className="text-4xl mt-8 md:text-5xl lg:text-7xl text-center lg:text-start font-bold">Manage Your<br /><span className="text-blue-600">Task</span> Quickly</h1>
                 <p className="mt-3 text-sm text-center lg:text-start text-slate-600 w-[400px] md:w-[600px] font-medium">Empower Your Productivity with Pro Tasker. Your Ultimate Task Management Solution! Streamline your workflow, stay organized, and achieve your goals effortlessly.</p>
--- src/pages/Home/Footer.jsx
+++ src/pages/Home/Footer.jsx
@@ -1,3 +1,5 @@
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+
 const Footer = () => {
     return (
         <div>
@@ -22,18 +24,25 @@ const Footer = () => {
                     <a className="link link-hover">Privacy policy</a>
                     <a className="link link-hover">Cookie policy</a>
                 </nav>
-                <form>
-                    <header className="footer-title">Newsletter</header>
-                    <fieldset className="form-control w-80">
-                        <label className="label">
-                            <span className="text-white label-text">Enter your email address</span>
-                        </label>
-                        <div className="join">
-                            <input type="text" placeholder="username@site.com" className="input input-bordered join-item" />
-                            <button className="btn btn-error text-white cursor-pointer join-item">Subscribe</button>
-                        </div>
-                    </fieldset>
-                </form>
+                <div>
+                    <form>
+                        <header className="footer-title">Newsletter</header>
+                        <fieldset className="form-control w-80">
+                            <label className="label">
+                                <span className="text-white label-text">Enter your email address</span>
+                            </label>
+                            <div className="join">
+                                <input type="text" placeholder="username@site.com" className="input input-bordered join-item" />
+                                <button className="btn btn-error text-white cursor-pointer join-item">Subscribe</button>
+                            </div>
+                        </fieldset>
+                    </form>
+                    <ul className="flex text-3xl gap-8 mt-3 cursor-pointer">
+                        <li><a href="https://www.facebook.com"><FaFacebook/></a></li>
+                        <li><a href="https://www.instagram.com"><FaInstagram/></a></li>
+                        <li><a href="https://twitter.com/?partner=opera15-other"><FaTwitter/></a></li>
+                    </ul>
+                </div>
             </footer>
         </div>
     );
